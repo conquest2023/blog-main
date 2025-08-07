095d8640cbc1ee4a18064065e3ec584ec3ea0e0e
conquest2023 <dlqns928@naver.com>
2025-08-07 17:09:09 +0900

    fix(argocd): remove jsonnet setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter', // CSS 변수 생성
+})
 
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
